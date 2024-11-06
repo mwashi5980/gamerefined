539a14402ebd1f1a06f99f97ac7918dc527ba17a
mwashi5980 <mwashi5980@inst.hcpss.org>
2024-11-06 15:48:35 +0000

    push2

--- main.py
+++ main.py
@@ -1,24 +1,41 @@
-import random
-def textfile(guess, score = 0):
+import random 
+def textfile():
     with open("random.txt", "w") as f:
         for _ in range(15):
             f.write(str(random.randint(0,1))+"\n")
     f.close()
-    with open("random.txt", "r") as f:
-        lines = f.readlines()
-        for x in range(_+1):
+def playerguess(score=0):
+    print("Its your Turn Player!")
+    with open("random.txt", "r") as p:
+        lines = p.readlines()
+        for x in range(15):
             list = int(lines[x])
-            try:
-                guess = int(input("Guess: "))
-                if guess != list:
-                    print(f"Incorrect!\tYour final score is {score}")
-                    break
-                else:
-                    score += 1
-                    print(f"Correct!\tYour current score is {score}")
-            except ValueError:
-                print("Not a number!")
-                score = 0
+            guess = int(input("Guess: "))
+            if guess !=list:
+                print(f"Incorrect\tYour final score is: {score}\n")
                 break
-    print("Game Over!")
-textfile(0)
+            else:
+                score += 1
+                print(f"Correct!\tYour current score is: {score}")
+        if score == 15:
+            print(f"Congrats you got a perfect score of {score}\n")
+    p.close()
+def AIguess(scoreAI=0):
+    print("Its your turn AI!")
+    with open("random.txt", "r") as AI:
+        lines = AI.readlines()
+        for x in range(15):
+            list = int(lines[x])
+            guess = (random.randint(0,1))
+            print(f"Guess: {guess}")
+            if guess != list:
+                print(f"Incorrect AI\tYour final score is: {scoreAI}\n")
+                break
+            else:
+                scoreAI += 1
+                print(f"Correct AI!\tYour current score is: {scoreAI}")
+        if scoreAI == 15:
+            print(f"Congrats AI you got a perfect score of {scoreAI}\n")
+    AI.close()
+playerguess()
+AIguess()
